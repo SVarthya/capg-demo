ca5e06de416fe0c6fd78c9f41f9c9ab08f5725ae
SVarthya <118422910+SVarthya@users.noreply.github.com>
2022-11-27 10:04:16 +0530

    Add files via upload

---
+++ config.py
@@ -0,0 +1,6 @@
+class Config:
+    ChromePath_Driver_Path=r'C:\Users\SRAVANI\Desktop\drivers.sel\chromedriver.exe'
+    Edge_Driver_Path=r"C:\Users\SRAVANI\Desktop\drivers.sel\msedgedriver.exe"
+    Firefox_Driver_Path=r"C:\Users\SRAVANI\Desktop\drivers.sel\geckodriver.exe"
+    URL="https://www.makemytrip.com/"
+    Data_path=r"C:\Users\SRAVANI\Desktop\makemytrip\sprint2.xlsx"
